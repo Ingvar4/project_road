f11a1b5e0af5f30d850932ae9d6d2d551581cf4e
Ingvar <jamessoer91@gmail.com>
2025-08-14 17:20:06 +0300

    roadmap

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+function App() {
+  return (
+    <div className="container bg-gray-100 mx-auto my-5 py-10">
+      <div className="flex flex-col md:flex-row justify-center items-center ">
+        
+        <h1 className="text-5xl">
+          Front-end Roadmap
+        </h1>
+
+      </div>
+    </div>
+  );
+}
+
+export default App
